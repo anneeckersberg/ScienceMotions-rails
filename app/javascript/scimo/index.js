54508c8571d8f5ac0d71d464ad9525c1df73e0ac
Anne Elisabeth <anne.eckersberg@gmail.com>
2018-08-21 13:51:37 +0200

    add CellsTissuesOrgans.css file in javascript/scimo/styles, import css file in ./index.js

--- app/javascript/scimo/index.js
+++ app/javascript/scimo/index.js
@@ -2,5 +2,6 @@ import "./styles/application.css";
 import "./styles/style.css";
 import "./styles/Indicators.css";
 import "./styles/DNA.css";
+import "./styles/CellsTissuesOrgans.css";
 
 import "./scripts/DNA.js";
